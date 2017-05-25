40d72f3569ede4412589d5e68d3c10e06d222c95
chinesejar <chinesejar@outlook.com>
2017-05-25 10:03:45 +0800

    mofify READEME

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # simulate-volte
 an android app for simulating sip packages by volte
 
+**界面**
 
-![simulate-volte](http://opf7qiimj.bkt.clouddn.com/simulate-volte.jpg "simulate-volte")
+<img src="http://opf7qiimj.bkt.clouddn.com/simulate-volte.jpg" width = "300" height="553" alt="图片名称" align=center />
--- app/src/main/java/com/example/chinesejar/sipdemo/PackageActivity.java
+++ app/src/main/java/com/example/chinesejar/sipdemo/PackageActivity.java
@@ -1,9 +1,8 @@
 package com.example.chinesejar.sipdemo;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 
 public class PackageActivity extends AppCompatActivity {
 
@@ -11,8 +10,6 @@ public class PackageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_package);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         initView();
     }
@@ -25,5 +22,4 @@ public class PackageActivity extends AppCompatActivity {
                 break;
         }
     }
-
 }
